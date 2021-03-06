65cac59c645d9957649aa25598cdb2a04241bcb7
ookami-kb <ookami.kb@gmail.com>
2021-03-06 22:55:48 +0100

    Fix warnings

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:storybook_flutter/storybook_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) => Storybook(
         children: [
