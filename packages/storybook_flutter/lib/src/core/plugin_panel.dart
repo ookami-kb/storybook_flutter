3ddf3e1f6f6bdeaf4863dcdbac674ab6a3a7886d
ookami-kb <ookami.kb@gmail.com>
2021-10-23 17:07:45 +0200

    update overlay

--- packages/storybook_flutter/lib/src/core/plugin_panel.dart
+++ packages/storybook_flutter/lib/src/core/plugin_panel.dart
@@ -16,7 +16,7 @@ class PluginPanel extends StatefulWidget {
 final overlayKey = GlobalKey<OverlayState>();
 
 class _PluginPanelState extends State<PluginPanel> {
-  OverlayEntry? _entry;
+  PluginOverlay? _overlay;
 
   OverlayEntry _createEntry(WidgetBuilder childBuilder) => OverlayEntry(
         builder: (context) => Stack(
@@ -43,6 +43,30 @@ class _PluginPanelState extends State<PluginPanel> {
         ),
       );
 
+  void _onPluginButtonPressed(Plugin p) {
+    p.onPressed?.call(context);
+    if (p.panelBuilder == null) return;
+
+    void insertOverlay() {
+      _overlay = PluginOverlay(
+        plugin: p,
+        entry: _createEntry((context) => p.panelBuilder!(context)),
+      );
+      overlayKey.currentState!.insert(_overlay!.entry);
+    }
+
+    if (_overlay != null) {
+      _overlay!.remove();
+      if (_overlay!.plugin != p) {
+        insertOverlay();
+      } else {
+        _overlay = null;
+      }
+    } else {
+      insertOverlay();
+    }
+  }
+
   @override
   Widget build(BuildContext context) => Wrap(
         runAlignment: WrapAlignment.center,
@@ -51,22 +75,19 @@ class _PluginPanelState extends State<PluginPanel> {
             .map(
               (p) => IconButton(
                 icon: p.icon!.call(context),
-                onPressed: () {
-                  p.onPressed?.call(context);
-                  if (p.panelBuilder == null) return;
-
-                  if (_entry != null) {
-                    _entry!.remove();
-                    _entry = null;
-                  } else {
-                    _entry = _createEntry(
-                      (context) => p.panelBuilder!(context),
-                    );
-                    overlayKey.currentState!.insert(_entry!);
-                  }
-                },
+                onPressed: () => _onPluginButtonPressed(p),
               ),
             )
             .toList(),
       );
+}
+
+@immutable
+class PluginOverlay {
+  const PluginOverlay({required this.entry, required this.plugin});
+
+  final OverlayEntry entry;
+  final Plugin plugin;
+
+  void remove() => entry.remove();
 }
