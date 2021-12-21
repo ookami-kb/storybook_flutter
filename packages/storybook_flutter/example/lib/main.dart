afafbcbd38446b733d2435b00ff1075b23acb08f
ookami-kb <ookami.kb@gmail.com>
2021-12-21 17:06:29 +0100

    docs: Update example

--- packages/storybook_flutter/example/lib/main.dart
+++ packages/storybook_flutter/example/lib/main.dart
@@ -42,9 +42,68 @@ class MyApp extends StatelessWidget {
             name: 'Scaffold',
             builder: (context) => Scaffold(
               appBar: AppBar(
-                title: const Text('Storybook'),
+                title: Text(
+                  context.knobs.text(
+                    label: 'Title',
+                    initial: 'Scaffold',
+                    description: 'The title of the app bar.',
+                  ),
+                ),
+                elevation: context.knobs.slider(
+                  label: 'AppBar elevation',
+                  initial: 4,
+                  min: 0,
+                  max: 10,
+                  description: 'Elevation of the app bar.',
+                ),
+                backgroundColor: context.knobs.options(
+                  label: 'AppBar color',
+                  initial: Colors.blue,
+                  description: 'Background color of the app bar.',
+                  options: const [
+                    Option(
+                      label: 'Blue',
+                      value: Colors.blue,
+                      description: 'Blue color',
+                    ),
+                    Option(
+                      label: 'Green',
+                      value: Colors.green,
+                      description: 'Green color',
+                    ),
+                  ],
+                ),
               ),
-              body: const Center(child: Text('Hello World!')),
+              body: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: List.generate(
+                    context.knobs.sliderInt(
+                      label: 'Items count',
+                      initial: 2,
+                      min: 1,
+                      max: 5,
+                      description: 'Number of items in the body container.',
+                    ),
+                    (_) => const Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Text('Hello World!'),
+                    ),
+                  ),
+                ),
+              ),
+              floatingActionButton: context.knobs.boolean(
+                label: 'FAB',
+                initial: true,
+                description: 'Show FAB button',
+              )
+                  ? FloatingActionButton(
+                      onPressed: () {},
+                      child: const Icon(Icons.add),
+                    )
+                  : null,
             ),
           ),
           Story(
