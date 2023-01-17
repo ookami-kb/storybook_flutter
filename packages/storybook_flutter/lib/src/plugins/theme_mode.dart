d38d99ae41d9724e8cd969c31ffcdf9f572257ca
Vitalij Vascenko <vetal.zt@gmail.com>
2023-01-17 12:21:39 +0100

    feat(storybook): [RND-104011] Remember light / dark mode settings

--- packages/storybook_flutter/lib/src/plugins/theme_mode.dart
+++ packages/storybook_flutter/lib/src/plugins/theme_mode.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storybook_flutter/src/plugins/plugin.dart';
 
 const themeModePlugin = Plugin(
@@ -29,36 +30,80 @@ void _onPressed(BuildContext context) {
   switch (context.read<ThemeModeNotifier>().value) {
     case ThemeMode.system:
       context.read<ThemeModeNotifier>().value = ThemeMode.light;
+      _saveThemeModeToPreferences(ThemeMode.light);
       break;
     case ThemeMode.light:
       context.read<ThemeModeNotifier>().value = ThemeMode.dark;
+      _saveThemeModeToPreferences(ThemeMode.dark);
       break;
     case ThemeMode.dark:
       context.read<ThemeModeNotifier>().value = ThemeMode.system;
+      _saveThemeModeToPreferences(ThemeMode.system);
       break;
   }
 }
 
-Widget _buildWrapper(BuildContext _, Widget? child) =>
-    ChangeNotifierProvider<ThemeModeNotifier>(
-      create: (_) => ThemeModeNotifier(ThemeMode.system),
-      child: Builder(
-        builder: (context) {
-          final themeMode = context.watch<ThemeModeNotifier>().value;
-          final brightness = themeMode == ThemeMode.system
-              ? MediaQuery.platformBrightnessOf(context)
-              : themeMode == ThemeMode.light
-                  ? Brightness.light
-                  : Brightness.dark;
+Future<ThemeMode> _getThemeModeFromPreferences() async {
+  final prefs = await SharedPreferences.getInstance();
+  final themeMode = prefs.getString('themeMode');
+  switch (themeMode) {
+    case 'system':
+      return ThemeMode.system;
+    case 'light':
+      return ThemeMode.light;
+    case 'dark':
+      return ThemeMode.dark;
+    default:
+      return ThemeMode.system;
+  }
+}
 
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(
-              platformBrightness: brightness,
-            ),
-            child: child ?? const SizedBox.shrink(),
-          );
-        },
-      ),
+Future<void> _saveThemeModeToPreferences(ThemeMode themeMode) async {
+  final prefs = await SharedPreferences.getInstance();
+  switch (themeMode) {
+    case ThemeMode.system:
+      await prefs.setString('themeMode', 'system');
+      break;
+    case ThemeMode.light:
+      await prefs.setString('themeMode', 'light');
+      break;
+    case ThemeMode.dark:
+      await prefs.setString('themeMode', 'dark');
+      break;
+  }
+}
+
+Widget _buildWrapper(BuildContext _, Widget? child) => FutureBuilder<ThemeMode>(
+      future: _getThemeModeFromPreferences(),
+      builder: (context, snapshot) {
+        final themeMode = snapshot.data ?? ThemeMode.system;
+
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return const Center(child: CircularProgressIndicator());
+          default:
+            return ChangeNotifierProvider<ThemeModeNotifier>(
+              create: (_) => ThemeModeNotifier(themeMode),
+              child: Builder(
+                builder: (context) {
+                  final themeMode = context.watch<ThemeModeNotifier>().value;
+                  final brightness = themeMode == ThemeMode.system
+                      ? MediaQuery.platformBrightnessOf(context)
+                      : themeMode == ThemeMode.light
+                          ? Brightness.light
+                          : Brightness.dark;
+
+                  return MediaQuery(
+                    data: MediaQuery.of(context).copyWith(
+                      platformBrightness: brightness,
+                    ),
+                    child: child ?? const SizedBox.shrink(),
+                  );
+                },
+              ),
+            );
+        }
+      },
     );
 
 /// Use this notifier to get the current theme mode.
