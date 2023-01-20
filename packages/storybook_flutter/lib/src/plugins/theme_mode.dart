f536927fd81003c0f0849052120f898483f96181
Vitalij Vascenko <vetal.zt@gmail.com>
2023-01-20 12:04:41 +0100

    Removed saving theme in the Storybook. Added callback on change.

--- packages/storybook_flutter/lib/src/plugins/theme_mode.dart
+++ packages/storybook_flutter/lib/src/plugins/theme_mode.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:storybook_flutter/src/plugins/plugin.dart';
 
-const themeModePlugin = Plugin(
-  icon: _buildIcon,
-  wrapperBuilder: _buildWrapper,
-  onPressed: _onPressed,
-);
+typedef ThemeCallback = void Function(ThemeMode themeMode);
+
+class ThemeModePlugin extends Plugin {
+  ThemeModePlugin({
+    ThemeMode? initialTheme,
+    ThemeCallback? onThemeChanged,
+  }) : super(
+          icon: _buildIcon,
+          wrapperBuilder: (context, widget) => _buildWrapper(
+            context,
+            widget,
+            initialTheme,
+          ),
+          onPressed: (context) => _onPressed(context, onThemeChanged),
+        );
+}
 
 Widget _buildIcon(BuildContext context) {
   final IconData icon;
@@ -26,84 +36,44 @@ Widget _buildIcon(BuildContext context) {
   return Icon(icon);
 }
 
-void _onPressed(BuildContext context) {
+void _onPressed(BuildContext context, ThemeCallback? onThemeChanged) {
   switch (context.read<ThemeModeNotifier>().value) {
     case ThemeMode.system:
       context.read<ThemeModeNotifier>().value = ThemeMode.light;
-      _saveThemeModeToPreferences(ThemeMode.light);
+      onThemeChanged?.call(ThemeMode.light);
       break;
     case ThemeMode.light:
       context.read<ThemeModeNotifier>().value = ThemeMode.dark;
-      _saveThemeModeToPreferences(ThemeMode.dark);
+      onThemeChanged?.call(ThemeMode.dark);
+
       break;
     case ThemeMode.dark:
       context.read<ThemeModeNotifier>().value = ThemeMode.system;
-      _saveThemeModeToPreferences(ThemeMode.system);
+      onThemeChanged?.call(ThemeMode.system);
       break;
   }
 }
 
-Future<ThemeMode> _getThemeModeFromPreferences() async {
-  final prefs = await SharedPreferences.getInstance();
-  final themeMode = prefs.getString('themeMode');
-  switch (themeMode) {
-    case 'system':
-      return ThemeMode.system;
-    case 'light':
-      return ThemeMode.light;
-    case 'dark':
-      return ThemeMode.dark;
-    default:
-      return ThemeMode.system;
-  }
-}
+Widget _buildWrapper(BuildContext _, Widget? child, ThemeMode? initialTheme) =>
+    ChangeNotifierProvider<ThemeModeNotifier>(
+      create: (_) => ThemeModeNotifier(initialTheme ?? ThemeMode.system),
+      child: Builder(
+        builder: (context) {
+          final themeMode = context.watch<ThemeModeNotifier>().value;
+          final brightness = themeMode == ThemeMode.system
+              ? MediaQuery.platformBrightnessOf(context)
+              : themeMode == ThemeMode.light
+                  ? Brightness.light
+                  : Brightness.dark;
 
-Future<void> _saveThemeModeToPreferences(ThemeMode themeMode) async {
-  final prefs = await SharedPreferences.getInstance();
-  switch (themeMode) {
-    case ThemeMode.system:
-      await prefs.setString('themeMode', 'system');
-      break;
-    case ThemeMode.light:
-      await prefs.setString('themeMode', 'light');
-      break;
-    case ThemeMode.dark:
-      await prefs.setString('themeMode', 'dark');
-      break;
-  }
-}
-
-Widget _buildWrapper(BuildContext _, Widget? child) => FutureBuilder<ThemeMode>(
-      future: _getThemeModeFromPreferences(),
-      builder: (context, snapshot) {
-        final themeMode = snapshot.data ?? ThemeMode.system;
-
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting:
-            return const Center(child: CircularProgressIndicator());
-          default:
-            return ChangeNotifierProvider<ThemeModeNotifier>(
-              create: (_) => ThemeModeNotifier(themeMode),
-              child: Builder(
-                builder: (context) {
-                  final themeMode = context.watch<ThemeModeNotifier>().value;
-                  final brightness = themeMode == ThemeMode.system
-                      ? MediaQuery.platformBrightnessOf(context)
-                      : themeMode == ThemeMode.light
-                          ? Brightness.light
-                          : Brightness.dark;
-
-                  return MediaQuery(
-                    data: MediaQuery.of(context).copyWith(
-                      platformBrightness: brightness,
-                    ),
-                    child: child ?? const SizedBox.shrink(),
-                  );
-                },
-              ),
-            );
-        }
-      },
+          return MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              platformBrightness: brightness,
+            ),
+            child: child ?? const SizedBox.shrink(),
+          );
+        },
+      ),
     );
 
 /// Use this notifier to get the current theme mode.
